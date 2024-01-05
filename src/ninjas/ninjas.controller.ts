96cef6734bea984db883889432e76fce9af78368
blurry <tamsilansari29@gmail.com>
2024-01-05 19:20:57 +0530

    added routes for ninja api

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { UpdateNinjaDto } from './dto/update-ninja.dto';
 
 @Controller('ninjas')
-export class NinjasController {}
+export class NinjasController {
+  @Get()
+  getNinjas(@Query('type') type: string) {
+    return [{type}];
+  }
+
+  @Get(':id')
+  getOneNinja(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post()
+  createNinja(@Body() createNinjaDto: CreateNinjaDto) {
+    return {
+      name: createNinjaDto.name
+    };
+  }
+
+  @Put(':id')
+  updatedNinja(@Param('id') id: string, @Body() updateNinjaDto: UpdateNinjaDto) {
+    return {};
+  }
+
+  @Delete(':id')
+  deleteNinja(@Param('id') id: string) {
+    return {};
+  }
+}
